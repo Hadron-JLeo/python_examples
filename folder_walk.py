03c945753258702b8ec61e5524b28c59aaee60c2
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2023-11-28 15:46:56 +0100

    Create folder_walk.py

---
+++ folder_walk.py
@@ -0,0 +1,13 @@
+import os
+
+def walk_folder_check(path, env_name)->str:
+    # Function to walk down a folder and check its folder
+    # List directories in path
+    folders = os.listdir(path)
+    res:str = ""
+
+    for folder in folders:
+        if env_name in os.listdir(folder):
+            res = folder
+    
+    except Exception as e: print(e)

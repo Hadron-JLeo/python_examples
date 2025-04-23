13d125f829bfe235677805a32966d10c16579b23
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2025-04-23 16:06:47 +0200

    Create step_recursion.py

---
+++ step_recursion.py
@@ -0,0 +1,16 @@
+# I created this as an example assignment for students
+
+def my_recursion(max_limit: int = 500):
+    """
+    A recursive function that performs step-incrementation
+    """
+    result = 1 # Step = 1, Start = 1
+
+    if not result == max_limit:
+        # As long as result has not reached the max_limit        
+        result += my_recursion(max_limit - result)
+
+    print(result) # Print every iteration
+    return result
+
+my_recursion()

d0b4404683a468d687bd4f335ec46f0743ac0a77
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2025-04-28 23:29:47 +0200

    Update step_recursion.py

--- step_recursion.py
+++ step_recursion.py
@@ -1,5 +1,26 @@
-# I created this as an example assignment for students
+# Interesting takes on step recursion
 
+
+
+# Counts up by using a counter variable: current
+def count_up(max_limit: int, current: int = 1) -> None:
+    if current > max_limit:
+        return
+    print(current)
+    count_up(max_limit, current + 1)
+
+
+# Counts up using nested, helper function
+def count_up_helper(max_limit: int) -> None:
+    def helper(current: int) -> None:
+        if current > max_limit:
+            return
+        print(current)
+        helper(current + 1)
+
+    helper(1)  # Start from 1
+
+# 'Magical' recursion by subtracting backwards and adding the to the result, only works if the result is a number that divides the max_limit cleanly!
 def my_recursion(max_limit: int = 500):
     """
     A 'magical' recursive function that performs step-incrementation

40cdb22e7d85edd2b61123f04de7bfe3522a5801
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2025-04-29 21:58:26 +0200

    Create decibel_check.py

---
+++ decibel_check.py
@@ -0,0 +1,22 @@
+""" "Decibel" Too loud exercise """
+
+
+def too_loud(level: int, earphones: bool) -> bool:
+    """Determine if the sound level is too loud, depending on earphone usage. True -> Too loud """
+    
+    if earphones:
+      # Check if we are above 120db while wearing earphones,
+      return 120 < level
+    else:
+      # Our limit without earphones is 70
+        return 70 < level
+
+def main()->None:
+  lvl = 121
+  ear = True # nose = taken
+
+  x = too_loud(lvl, ear)
+  print(x)
+
+if __name__ == "__main__":
+  main()

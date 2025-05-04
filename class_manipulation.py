4a563623ba62ffb3748a6c9a8b6f399f3fae7803
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2025-05-04 21:48:57 +0200

    Update class_manipulation.py

--- class_manipulation.py
+++ class_manipulation.py
@@ -14,3 +14,17 @@ print(c.wheels, c.__dict__)      # → 3, aber immer noch kein 'wheels' in c.__d
 
 c.wheels = 4         # Jetzt bekommt c ein eigenes wheels
 print(c.__dict__)    # → {'brand': 'Toyota', 'wheels': 4}
+
+
+
+### Wie entgegenwirken?
+class Car2:
+    __slots__ = ['brand']  # erlaubt nur das Attribut 'brand'
+
+    def __init__(self, brand: str):
+        self.brand = brand
+
+c2 = Car2("Toyota")
+c2.brand = "Honda"    # ok
+
+c2.wheels = 4         # ❌ AttributeError!

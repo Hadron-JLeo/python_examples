f171d8830f5131b32fbe893e9ecdfe052ea2e22b
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2025-05-04 21:39:56 +0200

    Update class_method_types.py

--- class_method_types.py
+++ class_method_types.py
@@ -1,25 +1,35 @@
-  
+""" Script to showcase the different types of methods a class can have """  
+
 class Maths(): 
 
     _count:int = 0
-
-    
 
     """ 
     We use a staticmethod when we know the function is semantically related to the class, 
     but does not need any information from the class or an instance
     """
+    def __init__(self, favorite_number:int)->None:
+        self.favorite_number = favorite_number
+        Maths._count += 1 
+
+  
     @staticmethod 
-    def add_numbers(num1:float, num2:float):
+    def add_numbers(num1:float, num2:float)->float:
         return num1 + num2
 
+  
     @classmethod
-    def 
+    def get_count(cls)->int:
+        return cls._count
 
-# Driver's code 
-if __name__ == "__main__": 
+  
+    def get_fav_num(self)->int:
+        return self.favorite_number
       
-    # Calling method of class 
-    # without creating instance 
+
+if __name__ == "__main__": 
+
     res = Maths.add_numbers(1, 2) 
+    my_math = Maths(13)
+    print(Maths.get_count())
     print("The result is", res) 

fb6349f8fa7baeebf64e03795d716cc25dd5a849
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2025-05-05 17:28:34 +0200

    Update n-sized-vector.py

--- n-sized-vector.py
+++ n-sized-vector.py
@@ -5,7 +5,10 @@ import math
 
 class Vector:
     def __init__(self, *values: float):
+        # Use a tuple to create a vector of n-dimensions
         self._values = tuple(float(v) for v in values)
+        
+        print(self) # Print to console once a new Vector is created
 
     def __len__(self):
         return len(self._values)
@@ -22,22 +25,21 @@ class Vector:
     def __eq__(self, other):
         return isinstance(other, Vector) and self._values == other._values
 
-    def __add__(self, other: Union[float, "Vector"]) -> "Vector":
-        if isinstance(other, (int, float)):
-            return Vector(*(v + other for v in self))
-        elif isinstance(other, Vector):
-            if len(self) != len(other):
-                raise ValueError("Vectors must be of the same dimension")
-            return Vector(*(a + b for a, b in zip(self, other)))
-        return NotImplemented
 
+    # Mathematical Methods from here
     def __sub__(self, other: Union[float, "Vector"]) -> "Vector":
         if isinstance(other, (int, float)):
+            # Check if the other element is of type int or float
             return Vector(*(v - other for v in self))
+        
         elif isinstance(other, Vector):
+            # If not int or float, check if they are of Vector type
             if len(self) != len(other):
+                # If the other element is of type Vector, they both need to have the same length
                 raise ValueError("Vectors must be of the same dimension")
+                
             return Vector(*(a - b for a, b in zip(self, other)))
+        
         return NotImplemented
 
     def magnitude(self) -> float:
@@ -49,3 +51,4 @@ class Vector:
             raise ZeroDivisionError("Cannot normalize a zero vector")
         return Vector(*(x / mag for x in self))
 
+# --- Class definition end for class Vector ---
